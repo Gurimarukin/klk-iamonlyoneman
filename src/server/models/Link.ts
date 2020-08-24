b0f459dba33cddeeb9a2b507c35a9fcb31b253bc
Gurimarukin <naxel@laposte.net>
2020-08-24 09:35:28 +0200

    Fix link model

--- src/server/models/Link.ts
+++ src/server/models/Link.ts
@@ -1,20 +1,28 @@
 import * as D from 'io-ts/lib/Decoder'
 
+import { pipe } from '../../shared/utils/fp'
+
 import { KlkPostId } from '../../shared/models/klkPost/KlkPostId'
 
 export namespace Link {
   export const decoder = D.type({
     kind: D.literal('t3'),
-    data: D.type({
-      id: KlkPostId.codec,
-      post_hint: D.string,
-      subreddit_name_prefixed: D.string,
-      author: D.string,
-      title: D.string,
-      created_utc: D.number,
-      permalink: D.string,
-      url: D.string,
-    }),
+    data: pipe(
+      D.type({
+        id: KlkPostId.codec,
+        subreddit_name_prefixed: D.string,
+        author: D.string,
+        title: D.string,
+        created_utc: D.number,
+        permalink: D.string,
+        url: D.string,
+      }),
+      D.intersect(
+        D.partial({
+          post_hint: D.string,
+        }),
+      ),
+    ),
   })
 }
 
