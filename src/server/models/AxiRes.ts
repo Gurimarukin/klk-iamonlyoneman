f3762d4c299bf276108a61f70d222bda1938360b
Gurimarukin <naxel@laposte.net>
2023-09-05 12:57:01 +0200

    remove axios, add got

--- src/server/models/AxiRes.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosResponse } from 'axios'
-
-export type AxiRes<A = unknown> = AxiosResponse<A>
-
-export namespace AxiRes {
-  export function map<A, B>(f: (a: A) => B): (fa: AxiRes<A>) => AxiRes<B> {
-    return fa => ({ ...fa, data: f(fa.data) })
-  }
-}
