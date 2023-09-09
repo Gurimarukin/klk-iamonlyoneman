877d580a2d69275d19cae1a3e852e7252b4547cb
Gurimarukin <naxel@laposte.net>
2023-09-09 16:25:59 +0200

    fix build?

--- src/server/Config.ts
+++ src/server/Config.ts
@@ -15,7 +15,7 @@ import { BooleanFromString, NonEmptyArrayFromString, URLFromString } from './uti
 
 const seqS = ValidatedNea.getSeqS<string>()
 
-export type Config = {
+type Config = {
   logLevel: LogLevelOrOff
   isDev: boolean
   pollOnStart: boolean
@@ -33,35 +33,37 @@ type DbConfig = {
   password: string
 }
 
-export namespace Config {
-  const parse = (dict: Partial<Dict<string, string>>): Try<Config> =>
-    parseConfig(dict)(r =>
-      seqS<Config>({
-        logLevel: r(LogLevelOrOff.decoder)('LOG_LEVEL'),
-        isDev: r(BooleanFromString.decoder)('IS_DEV'),
-        pollOnStart: r(BooleanFromString.decoder)('POLL_ON_START'),
-        pollEveryHours: r(MsDuration.fromStringDecoder)('POLL_EVERY_HOURS'),
-        port: r(NumberFromString.decoder)('SERVER_PORT'),
-        allowedOrigins: r(Maybe.decoder(NonEmptyArrayFromString.decoder(URLFromString.decoder)))(
-          'ALLOWED_ORIGINS',
-        ),
-        db: seqS<DbConfig>({
-          host: r(D.string)('DB_HOST'),
-          dbName: r(D.string)('DB_DB_NAME'),
-          user: r(D.string)('DB_USER'),
-          password: r(D.string)('DB_PASSWORD'),
-        }),
-
-        imagesDir: pipe(
-          r(D.string)('IMAGES_DIR'),
-          Either.map(dirName => pipe(Dir.of(__dirname), Dir.joinDir('..', '..', dirName))),
-        ),
+const parse = (dict: Partial<Dict<string, string>>): Try<Config> =>
+  parseConfig(dict)(r =>
+    seqS<Config>({
+      logLevel: r(LogLevelOrOff.decoder)('LOG_LEVEL'),
+      isDev: r(BooleanFromString.decoder)('IS_DEV'),
+      pollOnStart: r(BooleanFromString.decoder)('POLL_ON_START'),
+      pollEveryHours: r(MsDuration.fromStringDecoder)('POLL_EVERY_HOURS'),
+      port: r(NumberFromString.decoder)('SERVER_PORT'),
+      allowedOrigins: r(Maybe.decoder(NonEmptyArrayFromString.decoder(URLFromString.decoder)))(
+        'ALLOWED_ORIGINS',
+      ),
+      db: seqS<DbConfig>({
+        host: r(D.string)('DB_HOST'),
+        dbName: r(D.string)('DB_DB_NAME'),
+        user: r(D.string)('DB_USER'),
+        password: r(D.string)('DB_PASSWORD'),
       }),
-    )
 
-  export const load: IO<Config> = pipe(loadDotEnv, IO.map(parse), IO.chain(IO.fromEither))
+      imagesDir: pipe(
+        r(D.string)('IMAGES_DIR'),
+        Either.map(dirName => pipe(Dir.of(__dirname), Dir.joinDir('..', '..', dirName))),
+      ),
+    }),
+  )
 
-  export namespace Lens {
-    export const logLevel = MLens.fromProp<Config>()('logLevel')
-  }
+const load: IO<Config> = pipe(loadDotEnv, IO.map(parse), IO.chain(IO.fromEither))
+
+const Lens = {
+  logLevel: MLens.fromProp<Config>()('logLevel'),
 }
+
+const Config = { load, Lens }
+
+export { Config }
