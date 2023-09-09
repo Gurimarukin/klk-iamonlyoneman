a88110b0acc04f78d0ed6d62dd037a43c3c94e54
Gurimarukin <naxel@laposte.net>
2023-09-09 17:47:48 +0200

    fix build

--- src/client/utils/Config.ts
+++ src/client/utils/Config.ts
@@ -1,8 +1,24 @@
-const { API_HOST } = process.env
+import { pipe } from 'fp-ts/function'
+import * as D from 'io-ts/Decoder'
 
-if (API_HOST === undefined) {
-  // eslint-disable-next-line functional/no-throw-statements
-  throw Error('Missing env var API_HOST')
+import { Dict, Either, Try } from '../../shared/utils/fp'
+import { decodeError } from '../../shared/utils/ioTsUtils'
+
+type Config = D.TypeOf<typeof decoder>
+
+const decoder = D.struct({
+  apiHost: D.string,
+})
+
+// It's important to have process.env.ENV_VAR fully, as it is inlined by Parcel
+const inlined: Dict<string, string | undefined> = {
+  apiHost: process.env['API_HOST'],
 }
 
-export const Config = { apiHost: API_HOST }
+const Config: Config = pipe(
+  decoder.decode(inlined),
+  Either.mapLeft(decodeError('Config')(inlined)),
+  Try.getUnsafe,
+)
+
+export { Config }
