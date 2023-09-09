ab5de28f584caf726b15b0d3bb9f931d1a9c2232
Gurimarukin <naxel@laposte.net>
2023-09-09 16:31:44 +0200

    fix build?

--- src/client/utils/Config.ts
+++ src/client/utils/Config.ts
@@ -1,11 +1,11 @@
-export namespace Config {
-  export const apiHost: string = (() => {
-    const { API_HOST } = process.env
+const apiHost: string = (() => {
+  const { API_HOST } = process.env
 
-    if (API_HOST === undefined) {
-      // eslint-disable-next-line functional/no-throw-statements
-      throw Error('Missing env var API_HOST')
-    }
-    return API_HOST
-  })()
-}
+  if (API_HOST === undefined) {
+    // eslint-disable-next-line functional/no-throw-statements
+    throw Error('Missing env var API_HOST')
+  }
+  return API_HOST
+})()
+
+export const Config = { apiHost }
